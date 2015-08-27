b03a63dcea80eb2d9c3b9845d77deb58c2e63ab5
Mnwa <mihan@panfilov.biz>
2015-08-28 02:24:51 +0300

    get ips

--- Index.js
+++ Index.js
@@ -6,4 +6,9 @@ var http = require('http'),
 app = require('./lib/session.js')(app);
 app = require('./lib/proxy.js')(app);
 
+app.get('/ips', function(req, res){
+    console.log(req.session.ips);
+})
+    
+
 var server = require('./lib/server.js')(http, app, 8080);
