592acf9651e3c4fcea78c1da4eaef49386d19c74
Mnwa <mihan@panfilov.biz>
2015-08-28 02:22:14 +0300

    Add sessions and ip control

--- Index.js
+++ Index.js
@@ -2,6 +2,8 @@ var http = require('http'),
     express = require('express'),
     app = express();
 
+
+app = require('./lib/session.js')(app);
 app = require('./lib/proxy.js')(app);
 
 var server = require('./lib/server.js')(http, app, 8080);
