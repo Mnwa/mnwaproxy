f48f0daa3542e24a5b22001266b4b4389a8489c8
Mnwa <mihan@panfilov.biz>
2015-08-28 02:53:28 +0300

    Add clusters

--- Index.js
+++ Index.js
@@ -1,14 +1,22 @@
 var http = require('http'),
     express = require('express'),
-    app = express();
+    app = express(),
+    cluster = require('cluster'),
+    http = require('http'),
+    numCPUs = require('os').cpus().length;
 
 
-app = require('./lib/session.js')(app);
-app = require('./lib/proxy.js')(app);
+if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-app.get('/ips', function(req, res){
-    console.log(req.session.ips);
-})
-    
+    cluster.on('exit', function(worker, code, signal) {
+        cluster.fork();
+    });
+} else {
+    app = require('./lib/session.js')(app);
+    app = require('./lib/proxy.js')(app);
+    var server = require('./lib/server.js')(http, app, 8080);
+}
 
-var server = require('./lib/server.js')(http, app, 8080);
