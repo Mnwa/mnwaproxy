20df0d102304d168787a499ed665dd7e12d0a631
Mnwa <mihan@panfilov.biz>
2015-09-01 21:45:32 +0300

    New port

--- Index.js
+++ Index.js
@@ -17,5 +17,5 @@ if (cluster.isMaster) {
 } else {
 	app = require('./lib/session.js')(app);
 	app = require('./lib/proxy.js')(app);
-	var server = require('./lib/server.js')(http, app, 8080);
+	var server = require('./lib/server.js')(http, app, 8000);
 }
