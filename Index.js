615aec740542c3cbf312157f2cff612887617dbf
Mnwa <mihan@panfilov.biz>
2015-09-01 21:38:05 +0300

    Add +1 cpu
    Add +1 cpu

--- Index.js
+++ Index.js
@@ -1,22 +1,21 @@
 var http = require('http'),
-    express = require('express'),
-    app = express(),
-    cluster = require('cluster'),
-    http = require('http'),
-    numCPUs = require('os').cpus().length;
+	express = require('express'),
+	app = express(),
+	cluster = require('cluster'),
+	http = require('http'),
+	numCPUs = require('os').cpus().length;
 
 
 if (cluster.isMaster) {
-    for (var i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
+	for (var i = 0; i < numCPUs + 1; i++) {
+		cluster.fork();
+	}
 
-    cluster.on('exit', function(worker, code, signal) {
-        cluster.fork();
-    });
+	cluster.on('exit', function (worker, code, signal) {
+		cluster.fork();
+	});
 } else {
-    app = require('./lib/session.js')(app);
-    app = require('./lib/proxy.js')(app);
-    var server = require('./lib/server.js')(http, app, 8080);
+	app = require('./lib/session.js')(app);
+	app = require('./lib/proxy.js')(app);
+	var server = require('./lib/server.js')(http, app, 8080);
 }
-
