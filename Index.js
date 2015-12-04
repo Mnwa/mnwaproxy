3ba32c1102f7dadd148ad6fd3d79a3a592730020
Михаил <mnwa@localhost.localdomain>
2015-12-04 22:07:58 +0300

    1

--- Index.js
+++ Index.js
@@ -6,17 +6,21 @@ var http = require('http'),
 	numCPUs = require('os').cpus().length;
 
 
-if (cluster.isMaster) {
-	for (var i = 0; i < numCPUs + 1; i++) {
-		cluster.fork();
-	}
-
-	cluster.on('exit', function (worker, code, signal) {
-		console.error(" Process will be killed\n Error: %d\n %s\n", code, signal);
-		cluster.fork();
-	});
-} else {
-	app = require('./lib/session.js')(app);
-	app = require('./lib/proxy.js')(app);
-	var server = require('./lib/server.js')(http, app, 8000);
-}
+(function(){
+    'use strict'
+    if (cluster.isMaster) {
+        for (var i = 0; i < numCPUs + 1; i++) {
+            console.log(`Start server ${i}`)
+            cluster.fork();
+        }
+        cluster.on('exit', function (worker, code, signal) {
+            console.error(" Process will be killed\n Error: %d\n %s\n", code, signal);
+            console.log(`Start server ${i++}`)
+            cluster.fork();
+        });
+    } else {
+        app = require('./lib/session.js')(app);
+        app = require('./lib/proxy.js')(app);
+        var server = require('./lib/server.js')(http, app, 8000);
+    }
+})()
